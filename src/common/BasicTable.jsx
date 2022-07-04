3dab24036d9c210300ad3564eda6af3b2e718b8e
rahul <rahul.b@antino.io>
2022-07-04 17:22:57 +0530

    Added basic table

--- src/common/BasicTable.jsx
+++ src/common/BasicTable.jsx
@@ -3,14 +3,37 @@ import {
   useTableInstance,
   getCoreRowModel,
 } from "@tanstack/react-table";
+import { useState } from "react";
+
+import students from "../student.json";
 
 const table = createTable();
-console.log(table);
+const defaultData = [...students];
+
+const defaultColumns = [
+  table.createDataColumn("name", {
+    id: "name",
+  }),
+];
 
 const BasicTable = () => {
+  const [data, setData] = useState([...defaultData]);
+  const [columns, setColumns] = useState([...defaultColumns]);
+
+  const instance = useTableInstance(table, {
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  console.log(instance);
+
   return (
     <>
       <h2>Basic Table</h2>
+      <table>
+        <thead>{instance.getHeaderGroups()}</thead>
+      </table>
     </>
   );
 };
