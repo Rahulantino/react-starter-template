2d66832a5c336c84850c9b68982e2dc1cf417538
rahul <rahul.b@antino.io>
2022-07-04 14:02:01 +0530

    Added react table

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
-import { useState } from "react";
-import ErrorModal from "./common/Modal/ErrorModal";
+import React from "react";
 
 const App = () => {
-  const [open, setOpen] = useState(false);
-  const onClose = () => {
-    setOpen(false);
-  };
-
-  const handleSubmit = () => {
-    console.log("handleSubmit");
-    setOpen(false);
-  };
-
-  return (
-    <div>
-      <h2>React Responsive Modal</h2>
-      <button onClick={() => setOpen(true)}>open</button>
-      <ErrorModal open={open} onClose={onClose}>
-        <h2>hello world Responsive Modal </h2>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, beatae?
-        </p>
-        <button onClick={handleSubmit}>Submit</button>
-        <button>Cancel</button>
-      </ErrorModal>
-    </div>
-  );
+  return <></>;
 };
 
 export default App;
